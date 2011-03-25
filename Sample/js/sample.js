c3cd3df1303cf33c70882cd992720c2647f0d7e3
Eduardo Scoz <eduardoscoz@gmail.com>
2011-03-25 16:42:09 -0500

    Adding some extra samples

--- Sample/js/sample.js
+++ Sample/js/sample.js
@@ -5,6 +5,9 @@
 	"root": [
 		{ "elements":[
 				{ "id":"login", "type":"StringElement", "caption":"Login Form", "navigateto":"js/login.js" },
+				{ "id":"login", "type":"StringElement", "caption":"Another", "navigateto":"js/another.js" },
+				{ "id":"login", "type":"StringElement", "caption":"Countries", "navigateto":"js/countries.js" },
+				{ "id":"login", "type":"StringElement", "caption":"Form with values", "navigateto":"js/login.js" },
 				{ "id":"login", "type":"StringElement", "caption":"Form with values", "navigateto":"js/login.js" },
 				{ "type":"ActionElement", "caption":"Shop Popup", "action":"ShowPopup" }
 			]
