63231c4c839801f3917c84372689672976ff1f39
Eduardo Scoz <eduardoscoz@gmail.com>
2011-03-25 23:32:34 -0500

    Added test for MapElement

--- Sample/js/sample.js
+++ Sample/js/sample.js
@@ -8,7 +8,7 @@
 				{ "id":"login", "type":"StringElement", "caption":"Another", "navigateto":"js/another.js" },
 				{ "id":"login", "type":"StringElement", "caption":"Countries", "navigateto":"js/countries.js" },
 				{ "id":"login", "type":"StringElement", "caption":"Form with values", "navigateto":"js/login.js" },
-				{ "id":"login", "type":"StringElement", "caption":"Form with values", "navigateto":"js/login.js" },
+				{ "id":"login", "type":"MapElement", "caption":"Location", "value":"Sears", "lat":"41.878924", "lng":"-87.635987" },
 				{ "type":"ActionElement", "caption":"Shop Popup", "action":"ShowPopup" }
 			]
 		}
