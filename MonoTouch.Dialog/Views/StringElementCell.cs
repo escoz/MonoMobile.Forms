422ee5811e092aec8081988d29d1c929f77a9b8a
Eduardo Scoz <eduardoscoz@gmail.com>
2012-09-16 21:06:34 +0200

    Makes badges prettier

--- MonoTouch.Dialog/Views/StringElementCell.cs
+++ MonoTouch.Dialog/Views/StringElementCell.cs
@@ -10,12 +10,12 @@ namespace MonoTouch.Dialog
 		{
 			
 			BadgeLabel = new UILabel {
-				Font = UIFont.BoldSystemFontOfSize(12),
+				Font = UIFont.BoldSystemFontOfSize(13),
 				BackgroundColor = new UIColor(0.530f , 0.600f , 0.738f , 1.000f),
 				TextColor = UIColor.White,
 				TextAlignment = UITextAlignment.Center
 			};
-			BadgeLabel.Layer.CornerRadius = 6;
+			BadgeLabel.Layer.CornerRadius = 8;
 			
 			this.ContentView.Add(BadgeLabel);
 		}
@@ -36,7 +36,7 @@ namespace MonoTouch.Dialog
 			base.LayoutSubviews ();
 			if (_showBadge){
 				var size = BadgeLabel.StringSize(BadgeLabel.Text, BadgeLabel.Font);
-				BadgeLabel.Frame = new RectangleF(this.ContentView.Frame.Width-size.Width-18, this.ContentView.Frame.Height/2-((size.Height+4)/2), size.Width+12, size.Height+4);
+				BadgeLabel.Frame = new RectangleF(this.ContentView.Frame.Width-size.Width-22, this.ContentView.Frame.Height/2-((size.Height+4)/2), size.Width+16, size.Height+4);
 			}
 		}
 	}
